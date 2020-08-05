2c5001a72ffb6b3e5d1925edb8a410a4394c8ce0
Tarun Mahajan <tarunm201412@gmail.com>
2020-08-05 16:09:34 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -128,7 +128,7 @@ def parse_requirements(fname='requirements.txt', with_version=True):
 if __name__ == '__main__':
     setup(
         name='mmdet',
-        version=get_version(),
+#         version=get_version(),
         description='OpenMMLab Detection Toolbox and Benchmark',
         long_description=readme(),
         long_description_content_type='text/markdown',
